2edb8abebe2a6cce839a14b027ec6c468657e8fd
Tee Chor Yang <cyang0330@gmail.com>
2024-09-15 16:06:36 +0800

    Update Weiliang's Product Status

--- app/Livewire/ProductStatus.php
+++ app/Livewire/ProductStatus.php
@@ -8,6 +8,8 @@ use Livewire\Component;
 use Illuminate\View\View;
 use Filament\Tables\Table;
 use App\Models\OrderRequest;
+use Filament\Forms\Components\Select;
+use Filament\Tables\Actions\Action;
 use Filament\Forms\Contracts\HasForms;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Contracts\HasTable;
@@ -22,65 +24,61 @@ class ProductStatus extends Component implements HasForms, HasTable
     use InteractsWithForms;
     public function table(Table $table): Table
     {
-      
-      
+
+
         return $table
-       // ->query(
-      //       Product::query()
-      //           ->join('order_requests', 'order_requests.product_id', '=', 'products.id') // 关联 order_requests 表
-      //           ->join('categories', 'products.category_id', '=', 'categories.id') // 关联 categories 表
-      //           ->join('users', 'order_requests.customer_id', '=', 'users.id') // 关联 users 表获取 customer 名字
-      //           ->where('products.supplier_id', auth()->user()->id) // 检查当前用户是否是供货商
-      //           ->select(
-      //               'products.*', 
-      //               'order_requests.quantity', 
-      //               'order_requests.total_amount', 
-      //               'order_requests.status', 
-      //               'order_requests.approved_at',
-      //               'users.name as customer_name',  // 获取用户名字作为 customer_name
-      //               'products.name as product_name' // 获取产品名字作为 product_name
-      //           )
-      //   )
-      ->query(
-            OrderRequest::query()
-                ->join('products', 'order_requests.product_id', '=', 'products.id')
-                ->where('products.supplier_id', auth()->user()->id)
-                ->orderBy('order_requests.id', 'desc')
-        )
+            ->query(
+                OrderRequest::query()
+                    ->join('products', 'order_requests.product_id', '=', 'products.id')
+                    ->where('products.supplier_id', auth()->user()->id)
+                    ->orderBy('order_requests.id', 'desc')
+            )
             ->columns([
-                  TextColumn::make('customer.name')
-                      ->label('Customer')
-                      ->searchable()
-                      ->sortable(),
-                  TextColumn::make('product.name')
-                      ->label('Product')
-                      ->searchable()
-                      ->sortable(),
-                  TextColumn::make('quantity')
-                      ->label('Quantity')
-                      ->searchable()
-                      ->sortable(),
-                  TextColumn::make('total_amount')
-                      ->label('Total Amount')
-                      ->money('MYR')
-                      ->sortable(),
-                  TextColumn::make('approved_at')
-                      ->label('Approved At')
-                      ->dateTime()
-                      ->sortable(),
-              
-                
-                  SelectColumn::make('status')
+                TextColumn::make('customer.name')
+                    ->label('Customer')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('product.name')
+                    ->label('Product')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('quantity')
+                    ->label('Quantity')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('total_amount')
+                    ->label('Total Amount')
+                    ->money('MYR')
+                    ->sortable(),
+                TextColumn::make('approved_at')
+                    ->label('Approved At')
+                    ->dateTime()
+                    ->sortable(),
+
+                TextColumn::make('status')
                     ->label('Status')
-                    ->options(config('staticdata.order.order_status'))
-                    ->afterStateUpdated(function ($record, $state) {
-                        $record->update(['status' => $state]);
+                    ->badge()
+                    ->formatStateUsing(fn(string $state): string => ucwords(str_replace('_', ' ', $state))),
+            ])
+            ->actions([
+                Action::make('status')
+                    ->label('Update Status')
+                    ->icon('heroicon-m-check-badge')
+                    ->color('warning')
+                    ->fillForm(fn(OrderRequest $record): array => [
+                        'status' => $record->status,
+                    ])
+                    ->form([
+                        Select::make('status')
+                            ->label('Status')
+                            ->options(config('staticdata.order.order_status'))
+                            ->searchable(),
+                    ])
+                    ->action(function (array $data, OrderRequest $record): void {
+                        $record->status = $data['status'];
+                        $record->save();
                     }),
-                    
-
-                
             ]);
-
     }
 
     public function render(): View
--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -37,7 +37,7 @@
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link href="{{ route('product-status') }}" :active="request()->routeIs('product-status')">
-                        {{ __('Product-Status') }}
+                        {{ __('Product Status') }}
                     </x-nav-link>
                 </div>
                 @endif
